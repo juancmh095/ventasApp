e4eccab3d892ec1d45112b916636e03d31cca629
Juan Carlos Méndez Hernández <41524371+juancmh095@users.noreply.github.com>
2023-09-28 00:09:47 -0600

    cambios de fondo negrop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    window.matchMedia('(prefers-color-scheme: dark)');
+  }
 }
